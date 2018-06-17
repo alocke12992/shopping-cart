c3ae96739a2c40cb19bfe6c86b37c3ffa44682a7
Andrew Locke <alocke12992@gmail.com>
2018-06-17 11:26:52 -0600

    Builds routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
     #API ROUTES SHOULD GO HERE
+    resources :products, only: [:index]
+    resource :cart, only: [:show]
+    resources :order_items, only: [:create, :update, :destroy]
   end
 
   #Do not place any routes below this one
