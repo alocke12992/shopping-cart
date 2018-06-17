d29fd7ea761b2271b12b8fb26f98547a99b81151
Andrew Locke <alocke12992@gmail.com>
2018-06-17 11:35:45 -0600

    Fin order items controller

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,2 +1,7 @@
 class Api::CartsController < ApplicationController
+
+  def show
+    @order_items = current_order.order_items
+    render json: @order_items
+  end
 end
--- app/controllers/api/order_items_controller.rb
+++ app/controllers/api/order_items_controller.rb
@@ -1,2 +1,35 @@
 class Api::OrderItemsController < ApplicationController
+
+  def create
+    @order = current_order
+    @order_item = @order.order_items.new(order_item_params)
+    if @order.save
+      session[:order_id] = @order.id
+      render json: @order
+    else
+      render json: @order.errors
+    end
+  end
+
+  def update
+    @order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.update_attributes(order_item_params)
+    @order_items = @order.order_items
+    render json: @order_items
+  end 
+
+  def destroy
+    @order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.destroy
+    @order_items = @order.order_items
+  end 
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:quantity, :product_id)
+  end
+
 end
